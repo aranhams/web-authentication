c44e9e7b61d0b2d90daaa180fbb67fa78c807f94
aranhams <aranha.mss@gmail.com>
2021-02-27 01:54:07 -0300

    add unmarshal example

--- json enconding/main.go
+++ json enconding/main.go
@@ -20,10 +20,16 @@ func main() {
 	}
 
 	xp := []person{p1, p2}
-
 	bs, err := json.Marshal(xp)
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("PRINT JSON: ", string(bs))
+
+	xp2 := []person{}
+	err = json.Unmarshal(bs, &xp2)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("BACK INTO A GO DATA STRUCTURE: ", xp2)
 }
