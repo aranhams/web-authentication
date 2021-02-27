26a81343de45bd66c229359c8243770d4d520259
aranhams <aranha.mss@gmail.com>
2021-02-27 01:59:44 -0300

    set up server

--- json enconding/main.go
+++ json enconding/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"net/http"
 )
 
 type person struct {
@@ -11,25 +9,37 @@ type person struct {
 }
 
 func main() {
-	p1 := person{
-		First: "Jenny",
-	}
+	// p1 := person{
+	// 	First: "Jenny",
+	// }
 
-	p2 := person{
-		First: "James",
-	}
+	// p2 := person{
+	// 	First: "James",
+	// }
 
-	xp := []person{p1, p2}
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("PRINT JSON: ", string(bs))
+	// xp := []person{p1, p2}
+	// bs, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("PRINT JSON: ", string(bs))
 
-	xp2 := []person{}
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("BACK INTO A GO DATA STRUCTURE: ", xp2)
+	// xp2 := []person{}
+	// err = json.Unmarshal(bs, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+	// fmt.Println("BACK INTO A GO DATA STRUCTURE: ", xp2)
+
+	http.HandleFunc("/encode", encode)
+	http.HandleFunc("/decode", decode)
+	http.ListenAndServe(":8080", nil)
+}
+
+func encode(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func decode(w http.ResponseWriter, r *http.Request) {
+
 }
