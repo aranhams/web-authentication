2be5fddb7aafbe9f6ea92d59e57942265ca1414a
aranhams <aranha.mss@gmail.com>
2021-03-02 19:15:35 -0300

    get the authenticated http client

--- oauth2/main.go
+++ oauth2/main.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
 )
 
-func getEnv() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error to loading .evn file")
-	}
+// func getEnv() {
+// 	err := godotenv.Load(".env")
+// 	if err != nil {
+// 		log.Fatal("Error to loading .evn file")
+// 	}
+// }
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "",
+	ClientSecret: "",
+	Endpoint:     github.Endpoint,
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/oauth/github", startGithubOauth)
+	http.HandleFunc("/oauth/receive", completeGithubOauth)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -42,13 +46,33 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func startGithubOauth(w http.ResponseWriter, r *http.Request) {
-	getEnv()
-	var githubOauthConfig = &oauth2.Config{
-		ClientID:     os.Getenv("CLIENTID"),
-		ClientSecret: os.Getenv("CLIENTSECRET"),
-		Endpoint:     github.Endpoint,
-	}
+	// getEnv()
+	// var githubOauthConfig = &oauth2.Config{
+	// 	ClientID:     os.Getenv("CLIENTID"),
+	// 	ClientSecret: os.Getenv("CLIENTSECRET"),
+	// 	Endpoint:     github.Endpoint,
+	// }
 
 	redirectURL := githubOauthConfig.AuthCodeURL("0000")
 	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func completeGithubOauth(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	state := r.FormValue("state")
+
+	if state != "0000" {
+		http.Error(w, "State is incorrect", http.StatusBadRequest)
+		return
+	}
+
+	token, err := githubOauthConfig.Exchange(r.Context(), code)
+	if err != nil {
+		http.Error(w, "Coudn't login", http.StatusInternalServerError)
+		return
+	}
+
+	ts := githubOauthConfig.TokenSource(r.Context(), token)
+	client := oauth2.NewClient(r.Context(), ts)
+
 }
