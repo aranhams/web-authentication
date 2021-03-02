5b3738ed191ae49a2a20ccadc510251a9f90d1ad
aranhams <aranha.mss@gmail.com>
2021-03-02 17:45:14 -0300

    starting oauth2 example :rocket:

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/joho/godotenv v1.3.0
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
 )
--- oauth2/main.go
+++ oauth2/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
 )
+
+func getEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error to loading .evn file")
+	}
+}
 
 func main() {
 	http.HandleFunc("/", index)
@@ -17,7 +30,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>HMAC Example</title>
+		<title>OAUHT2 Example</title>
 	</head>
 	<body>
 		<form action="/oauth/github" method="POST">
@@ -29,5 +42,13 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func startGithubOauth(w http.ResponseWriter, r *http.Request) {
+	getEnv()
+	var githubOauthConfig = &oauth2.Config{
+		ClientID:     os.Getenv("CLIENTID"),
+		ClientSecret: os.Getenv("CLIENTSECRET"),
+		Endpoint:     github.Endpoint,
+	}
 
+	redirectURL := githubOauthConfig.AuthCodeURL("0000")
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
 }
